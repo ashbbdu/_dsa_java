2db46e56e3c218cde2923227a17d0dc5c9b8f1c1
ashbbdu <ashishsrivastava7007@gmail.com>
2026-04-07 17:08:43 +0530

    RemoveElement

--- src/Arrays/Questions/RemoveElement.java
+++ src/Arrays/Questions/RemoveElement.java
@@ -1,7 +1,51 @@
 package Arrays.Questions;
 
-public class RemoveElement {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class RemoveElement {
+// Brute
+//    public static int removeElement(int[] nums, int val) {
+//        int start = 0;
+//        int end = nums.length - 1;
+//        int temp;
+//        while(start < end) {
+//            if(nums[start] == val) {
+//                temp = nums[start];
+//                nums[start] = nums[end];
+//                nums[end] = temp;
+//                end--;
+//            }
+//          else {
+//                start++;
+//            }
+//        }
+//
+//        System.out.println(Arrays.toString(nums));
+//        int count = 0;
+//        for(int i = 0 ; i < nums.length ; i++) {
+//            if(nums[i] != val) {
+//                count++;
+//            }
+//        }
+//        return count;
+//    }
+
+//    Optimal
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+        for(int i = 0; i < nums.length ; i++) {
+            if(nums[i] == val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {3,2,2,3};
+        int ans = removeElement(arr , 3);
+        System.out.println(ans);
     }
 }
