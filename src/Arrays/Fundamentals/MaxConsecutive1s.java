f9900f9120c5ed4a9d909b7c53ae6c1e283fcf74
ashbbdu <ashishsrivastava7007@gmail.com>
2026-04-27 23:07:18 +0530

    findMaxConsecutiveOnes

---
+++ src/Arrays/Fundamentals/MaxConsecutive1s.java
@@ -0,0 +1,22 @@
+package Arrays.Fundamentals;
+
+public class MaxConsecutive1s {
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int maxCount = 0;
+        for(int i = 0 ; i < nums.length ; i++) {
+            if(nums[i] == 1) {
+                count++;
+                maxCount = Math.max(count , maxCount);
+            } else {
+                count = 0;
+            }
+        }
+        return maxCount;
+    }
+    public static void main(String[] args) {
+        int [] arr = {1, 1, 0, 0, 1, 1, 1, 0};
+        System.out.println(findMaxConsecutiveOnes(arr));
+    }
+}
