9b714e69cab7c63e6a572af6e40cf77a7f14a7d1
ashbbdu <ashishsrivastava7007@gmail.com>
2026-04-29 00:35:42 +0530

    moveZeroesOptimal

--- src/Arrays/LogicBuilding/Move0sToEnd.java
+++ src/Arrays/LogicBuilding/Move0sToEnd.java
@@ -3,7 +3,7 @@ package Arrays.LogicBuilding;
 import java.util.Arrays;
 
 public class Move0sToEnd {
-    public static void moveZeroes(int[] nums) {
+    public static void moveZeroesBrute(int[] nums) {
 //        count zeros
         int zeroCount = 0;
         for(int i = 0 ; i < nums.length ; i++) {
@@ -24,9 +24,27 @@ public class Move0sToEnd {
         }
 
     }
+
+    public static void moveZeroesOptimal(int[] nums) {
+
+        int i = 0;
+        int  temp;
+        for (int j = 1 ; j < nums.length  ; j++) {
+
+            if(nums[j] != 0) {
+                temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+
+                i++;
+            }
+        }
+
+    }
     public static void main(String[] args) {
-        int [] nums = {5};
-        moveZeroes(nums);
+        int [] nums = {4, 0, 5, 0, 0, 3, 0, 1, 0, 2, 7};
+//        moveZeroesBrute(nums);
+        moveZeroesOptimal(nums);
         System.out.println(Arrays.toString(nums));
     }
 }
