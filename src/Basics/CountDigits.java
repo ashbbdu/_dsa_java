2e6ddeb72f3eb54664bb3fc89fd66754d6034e7b
ashbbdu <ashishsrivastava7007@gmail.com>
2026-05-25 23:35:32 +0530

    Basic Maths : countDigit

---
+++ src/Basics/CountDigits.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class CountDigits {
+    public static int countDigit(int n) {
+        if(n == 0) return 1;
+        int count = 0;
+        while (n > 0) {
+            count = count + 1;
+            n = n  / 10;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        System.out.println(countDigit(456));
+    }
+}
