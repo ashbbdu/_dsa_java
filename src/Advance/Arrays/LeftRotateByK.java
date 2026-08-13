4c157313f9118067d77d5fd91c3be76267ecc29e
ashbbdu <ashishsrivastava7007@gmail.com>
2026-08-13 23:53:39 +0530

    ReverseLeftByKOptimal

--- src/Advance/Arrays/LeftRotateByK.java
+++ src/Advance/Arrays/LeftRotateByK.java
@@ -4,11 +4,12 @@ import java.util.Arrays;
 
 public class LeftRotateByK {
     public static void main(String[] args) {
-        System.out.println(2%6);
-        System.out.println(8%6);
+//        System.out.println(2%6);
+//        System.out.println(8%6);
 
-        int k = 2;
+
         int [] nums = {1, 2, 3, 4, 5, 6};
+        int k = 2;
         k = k % nums.length;
         int [] temp = new int[k];
 
--- src/Advance/Arrays/ReverseLeftByKOptimal.java
+++ src/Advance/Arrays/ReverseLeftByKOptimal.java
@@ -1,4 +1,34 @@
 package Advance.Arrays;
 
+import java.util.Arrays;
+
 public class ReverseLeftByKOptimal {
+
+
+    public static void reverseArray (int [] nums , int start , int end) {
+        int temp;
+        while (start < end) {
+            temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {1, 2, 3, 4, 5, 6};
+        int k = 2;
+        k = k % nums.length;
+
+        reverseArray(nums , 0 , k - 1);
+        reverseArray(nums , k , nums.length - 1);
+
+        reverseArray(nums , 0 , nums.length -1);
+
+
+
+        System.out.println(Arrays.toString(nums));
+
+    }
 }
