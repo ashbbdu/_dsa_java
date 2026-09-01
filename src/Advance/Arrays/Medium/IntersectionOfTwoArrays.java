753a50d8f826573e6f01e77747b505c012f957f9
ashbbdu <ashishsrivastava7007@gmail.com>
2026-09-01 23:59:38 +0530

    questions

---
+++ src/Advance/Arrays/Medium/IntersectionOfTwoArrays.java
@@ -0,0 +1,21 @@
+package Advance.Arrays.Medium;
+
+import java.util.Arrays;
+
+public class IntersectionOfTwoArrays {
+
+    private int[] intersectionBrute(int[] nums1, int[] nums2) {
+
+    }
+
+    public static void main(String[] args) {
+        int [] nums1 = {3, 4, 6, 7, 9, 9};
+        int [] nums2 = {1, 5, 7, 8, 8};
+
+        System.out.println(Arrays.toString(intersectionBrute(nums1 , nums2)));
+
+//        System.out.println(Arrays.toString(unionOptimal(nums1 , nums2)));
+    }
+
+
+}
